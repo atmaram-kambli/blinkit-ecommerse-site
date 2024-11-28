c1cf93cffcd643b4ae3fa88c7264518b93c9c0e1
Atmaram Kambli <engr.atmaram.kambli22@gmail.com>
2024-11-28 17:35:21 +0530

    Add the login and logut APIs

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,7 +1,10 @@
 import UserModel from '../models/user.model.js';
 import sendEmail from '../config/sendEmail.js';
 import bcryptjs from 'bcryptjs'
+
 import verifyEmailTemplate from '../utils/verifyEmailTemplate.js';
+import generatedAccessToken from '../utils/generateAccessToken.js';
+import genertedRefreshToken from '../utils/generateRefreshToken.js';
 
 
 
@@ -11,7 +14,7 @@ export async function regiterUserController(req, res) {
         const {name, email, password} = req.body;
 
         if(!name || !email || !password) {
-            return res.status(500).json({
+            return res.status(400).json({
                 message: "Provide valid credentials!",
                 error: true,
                 success: false,
@@ -20,7 +23,7 @@ export async function regiterUserController(req, res) {
 
         const user = await UserModel.findOne({email});
         if(user) {
-            return res.status(500).json({
+            return res.status(400).json({
                 message: "Already registered User! Try Login",
                 error: true,
                 success: false,
@@ -39,17 +42,17 @@ export async function regiterUserController(req, res) {
         const newUser = await UserModel(payload);
         const save = await newUser.save();
 
-        const verity_page_url = `${process.env.FRONTEND_URL}/verify-email?code=${save._id}`;
+        const verity_page_url = `${process.env.FRONTEND_URL}/verify-email?code=${save?._id}`;
         // console.log(verity_page_url);
 
-        const verifyEmail = sendEmail({
+        const verifyEmail = await sendEmail({
             sendTo: email,
             subject: "Verify Email from BlinkIt",
             html: verifyEmailTemplate({name, verity_page_url})
         })
 
         return res.status(200).json({
-            message: "User register successfully!",
+            message: "User is registered successfully!",
             error: false,
             success: true,
             data: save,
@@ -73,7 +76,7 @@ export async function verifyEmailController(req, res) {
 
         if(!findUser) {
             return res.status(400).json({
-                message: "Bad Request!",
+                message: "Bad res!",
                 error: true,
                 success: false,
             });
@@ -97,3 +100,109 @@ export async function verifyEmailController(req, res) {
     }
 }
 
+// 3. User Login Controller
+export async function loginController(req,res){
+    try {
+        const { email , password } = req.body
+
+        if(!email || !password){
+            return res.status(400).json({
+                message : "Invalid crendentials!",
+                error : true,
+                success : false
+            })
+        }
+
+        const user = await UserModel.findOne({ email })
+
+        if(!user){
+            return res.status(400).json({
+                message : "User is not registered. Try SignUp!",
+                error : true,
+                success : false
+            })
+        }
+
+        if(user.status !== "Active"){
+            return res.status(400).json({
+                message : "Access Denied!",
+                error : true,
+                success : false
+            })
+        }
+
+        const checkPassword = await bcryptjs.compare(password,user.password)
+
+        if(!checkPassword){
+            return res.status(400).json({
+                message : "Wrong crendentials!",
+                error : true,
+                success : false
+            })
+        }
+
+        const accesstoken = await generatedAccessToken(user._id)
+        const refreshToken = await genertedRefreshToken(user._id)
+
+        const updateUser = await UserModel.findByIdAndUpdate(user?._id,{
+            last_login_date : new Date()
+        })
+
+        const cookiesOption = {
+            httpOnly : true,
+            secure : true,
+            sameSite : "None"
+        }
+        res.cookie('accessToken',accesstoken,cookiesOption)
+        res.cookie('refreshToken',refreshToken,cookiesOption)
+
+        return res.json({
+            message : "User is Logged in Successfully!",
+            error : false,
+            success : true,
+            data : {
+                accesstoken,
+                refreshToken
+            }
+        })
+
+    } catch (error) {
+        return res.status(500).json({
+            message : error.message || error,
+            error : true,
+            success : false
+        })
+    }
+}
+
+// 4. User logout controller
+export async function logoutController(req,res){
+    try {
+        const userid = req.userId
+
+        const cookiesOption = {
+            httpOnly : true,
+            secure : true,
+            sameSite : "None"
+        }
+
+        res.clearCookie("accessToken",cookiesOption)
+        res.clearCookie("refreshToken",cookiesOption)
+
+        const removeRefreshToken = await UserModel.findByIdAndUpdate(userid,{
+            refresh_token : ""
+        })
+
+        return res.json({
+            message : "User is Logged out successfully!",
+            error : false,
+            success : true
+        })
+    } catch (error) {
+        return res.status(500).json({
+            message : error.message || error,
+            error : true,
+            success : false
+        })
+    }
+}
--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,9 +1,12 @@
 import { Router } from 'express';
-import { regiterUserController, verifyEmailController } from '../controllers/user.controller.js';
+import { loginController, logoutController, regiterUserController, verifyEmailController } from '../controllers/user.controller.js';
+import auth from '../middlewares/auth.js';
 
 const router = Router();
 
 router.post('/register', regiterUserController);
 router.post('/verify-email', verifyEmailController);
+router.post('/login', loginController);
+router.get('/logout', auth, logoutController);
 
 export default router;
